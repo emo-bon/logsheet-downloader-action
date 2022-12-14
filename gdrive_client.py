1d8a7cad4791b330cf7b1f852abf522815ca3949
bulricht <108867473+bulricht@users.noreply.github.com>
2022-12-14 17:01:20 +0100

    refactor get_xlsx

--- gdrive_client.py
+++ gdrive_client.py
@@ -1,46 +1,36 @@
 import os
 import pandas as pd
-import requests
 import shutil
-import uritemplate
 import uuid
 import yaml
+from pyedm.gg import get_xlsx
 
-def get_xlsx(doc_id, path):
-    ut = "https://docs.google.com/spreadsheets/d/{id}/export{?format,id}"
-    url = uritemplate.expand(ut, {"id": doc_id, "format": "xlsx"})
-    r = requests.get(url, allow_redirects=True)
+GITHUB_WORKSPACE = "/github/workspace"
+
+with open(os.path.join(GITHUB_WORKSPACE, "config", "google-docs.yml"), "r", encoding="utf-8") as f:
+    config = yaml.load(f.read(), Loader=yaml.BaseLoader)
+
+for habitat in ("water", "sediment"):
+    url = config[habitat]
     
-    with open(os.path.join(path, f"{doc_id}.xlsx"), "wb") as f:
-        f.write(r.content)
+    if not url.startswith("https://"):  # else url = NaN
+        continue
 
-if __name__ == "__main__":
-    GITHUB_WORKSPACE = "/github/workspace"
+    doc_id = url.split("/")[5]
+    temp_folder_name = str(uuid.uuid1())
+    path_xlsx = os.path.join(GITHUB_WORKSPACE, temp_folder_name)
     
-    with open(os.path.join(GITHUB_WORKSPACE, "config", "google-docs.yml"), "r", encoding="utf-8") as f:
-        config = yaml.load(f.read(), Loader=yaml.BaseLoader)
+    if not os.path.exists(path_xlsx):
+        os.makedirs(path_xlsx)
 
-    for habitat in ("water", "sediment"):
-        url = config[habitat]
-        
-        if not url.startswith("https://"):
-            continue
+    get_xlsx(os.path.join(path_xlsx, f"{doc_id}.xlsx"), doc_id)
+    xlsx = pd.read_excel(os.path.join(path_xlsx, f"{doc_id}.xlsx"), sheet_name=None)
+    path_csv = os.path.join(GITHUB_WORKSPACE, "downloads", "gdoc-csv")
+    
+    if not os.path.exists(path_csv):
+        os.makedirs(path_csv)
 
-        doc_id = url.split("/")[5]
-        tmp = str(uuid.uuid1())
-        path_xlsx = os.path.join(GITHUB_WORKSPACE, tmp)
-        
-        if not os.path.exists(path_xlsx):
-            os.makedirs(path_xlsx)
+    for sheet in ("observatory", "sampling", "measured"):
+        xlsx[sheet].to_csv(os.path.join(path_csv, f'{habitat}_{sheet}.csv'), index=False)
 
-        get_xlsx(doc_id, path_xlsx)
-        xlsx = pd.read_excel(os.path.join(path_xlsx, f"{doc_id}.xlsx"), sheet_name=None)
-        path_csv = os.path.join(GITHUB_WORKSPACE, "downloads", "gdoc-csv")
-        
-        if not os.path.exists(path_csv):
-            os.makedirs(path_csv)
-
-        for sheet in ("observatory", "sampling", "measured"):
-            xlsx[sheet].to_csv(os.path.join(path_csv, f'{habitat}_{sheet}.csv'), index=False)
-
-        shutil.rmtree(path_xlsx)
+    shutil.rmtree(path_xlsx)
